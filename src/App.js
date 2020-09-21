8fbe7eccd7b1da1bcab0ccd4b40c4bb4596f20b3
Naveed ul Hassan Malik <naveed@appmakers.pk>
2020-09-21 18:24:47 +0500

    use context

--- src/App.js
+++ src/App.js
@@ -1,26 +1,105 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useContext, useState} from "react";
+import "./App.css";
+
+// create a context / store
+const ThemeContext = React.createContext();
+const ThemeProvider = function (props) {
+    let [theme, setTheme] = useState('light');
+
+    let context = {
+        theme,
+        toggle() {
+            setTheme(theme => theme === 'light' ? 'dark' : 'light');
+        }
+    }
+
+    return (
+        <ThemeContext.Provider value={context}>
+            {props.children}
+        </ThemeContext.Provider>
+    );
+}
+
+// create a second context / store
+const UserContext = React.createContext();
+const UserProvider = function (props) {
+    let [name, setName] = useState('Naveed');
+
+    let context = {
+        name,
+        setName
+    }
+
+    return (
+        <UserContext.Provider value={context}>
+            {props.children}
+        </UserContext.Provider>
+    );
+}
+
+// reading the context data in function component
+function ChildComp1() {
+    let theme = useContext(ThemeContext);
+    let user = useContext(UserContext);
+    return (
+        <div>
+            <h3>Child 1 {theme.theme}</h3>
+            <h3>Current User is: {user.name}</h3>
+        </div>
+    );
+}
+
+// reading the context data in class component
+class ChildComp2 extends React.Component {
+    static contextType = ThemeContext;
+
+    render() {
+        return (
+            <h3>Child 2 {this.context.theme}</h3>
+        );
+    }
+}
+
+// modifying the context data
+function Toggler() {
+    return (
+        <ThemeContext.Consumer>
+            {theme => (<button onClick={theme.toggle}>Toggle Theme</button>)}
+        </ThemeContext.Consumer>
+    )
+}
+
+function NameChanger(props) {
+    return (
+        <UserContext.Consumer>
+            {
+                user => {
+                    function update(e) {
+                        user.setName(e.target.value)
+                    }
+                    return (
+                        <div>
+                            <input type="text" onInput={update}/>
+                        </div>
+                    )
+                }
+            }
+        </UserContext.Consumer>
+    )
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <ThemeProvider>
+            <UserProvider>
+                <h1>Parent</h1>
+                <Toggler/>
+                <ChildComp1/>
+                <ChildComp2/>
+                <NameChanger/>
+            </UserProvider>
+        </ThemeProvider>
+    );
 }
 
 export default App;
